ba39b5d29361c30f3e861999e8c03d0c9d3d1028
Yubisel <yubiselv@gmail.com>
2020-01-02 14:10:04 -0300

    creada la estructura base de la aplicacion

--- src/server.js
+++ src/server.js
@@ -1,24 +1,23 @@
-const express = require('express')
-const logger = require('morgan')
-// const path = require('path')
+const express = require('express');
+const logger = require('morgan');
+const path = require('path');
 
 
 //Initializations
 const app = express();
 
 //Settings
-app.set('port', process.env.PORT || 3000)
+app.set('port', process.env.PORT || 3000);
 
 //Middlewares
 app.use(logger('dev'));
-
-//Statics
-// app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.json());
 
 //Routes
-app.get('/', (req, res) => {
-    res.send('Server working')
-});
+app.use('/categories', require('./routes/Categories'));
+
+//Statics
+app.use(express.static(path.join(__dirname, 'public')));
 
 
-app.listen(app.get('port'), () => console.log('Server listening on port', app.get('port')))
+app.listen(app.get('port'), () => console.log('Server listening on port', app.get('port')));
